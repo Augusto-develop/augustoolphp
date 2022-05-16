d0d4ec8c584907271111af19ab157fcef5f65ad5
Augusto <augustogomes0822@gmail.com>
2022-05-16 19:41:52 -0300

    boleto tool

---
+++ src/BoletoTool.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace AugusToolPHP;
+
+
+class BoletoTool
+{
+   public static function formatNossoNumero($banco, $nossoNumComp, $dvNossoNum, $layout, $carteiraGeracao)
+   {
+      $explodeBanco = explode("-", $banco);
+      $codBanco = $explodeBanco[0];
+
+      switch ($codBanco){
+         case '104'://CAIXA
+            if($layout == 'SIGCB240' || $layout == 'SIGCB400'){
+               return substr($nossoNumComp, 1, 17) . '-' .$dvNossoNum;
+            }else{
+               return "";
+            }
+            break;
+         case '341'://ITAU
+            return $carteiraGeracao . "/" .substr($nossoNumComp, 10, 8) . '-' .$dvNossoNum;
+         case '237'://BRADESCO
+            return $carteiraGeracao . "/" . substr($nossoNumComp, 7, 11) . '-' .$dvNossoNum;
+         case '001'://BANCO DO BRASIL
+            return substr($nossoNumComp, 1, 17);
+         case '033'://SANTANDER
+            return substr($nossoNumComp, 5, 13);
+
+      }
+   }
+}
